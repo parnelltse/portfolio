a1a0e47c7b67ae8917da4c07d62da1717dd70701
parnelltse <parnell.tse@gmail.com>
2025-01-26 01:42:22 -0800

    styling eirene

--- pages/Eirene.js
+++ pages/Eirene.js
@@ -16,7 +16,7 @@ export default function Eirene() {
             </Head>
             <div className={styles.homeContainer}>
                 <Header />
-                <main className={styles.content}>
+                <main>
                     <div className={styles.imageContainer}>
                         <Image
                             src="/mag mock cover.png"
@@ -26,50 +26,50 @@ export default function Eirene() {
                             className={styles.mag}
                         />
                     </div>
-                    <h0 className={styles.title}>Eirene</h0>
-                    <h4 className={styles.subhead}>Designing and curating an Art Deco-inspired interior design magazine</h4>
-                    <p className={styles.emoticon}>(∩｀-´)⊃ ☆ﾟ.*･｡ﾟ</p>
-                    <div className={styles.textAndRoleContainer}>
-                        <p className={styles.body_text}>Eirene is an interior design magazine created to showcase the
-                            architectural and design style of Art Deco. Drawing from the monotony
-                            of boring modern interiors, this project
-                            aims to showcase a different architectural
-                            and design style while keeping readers engaged. The publication showcases the different textures,
-                            colours, and layouts of Art Deco. By bringing together all those design elements
-                            and combining them into one magazine, it keeps the reader interested and engaged while
-                            reading the publication.
-                        </p>
-                        <InfoBlock className={styles.role} items={[{
-                            title: "Year", bodyText: "September 2024 – October 2024"
-                        }, {
-                            title: "Role", bodyText: "Designer, Researcher"
-                        }, {
-                            title: "Skills", bodyText: "InDesign, Photoshop, Text layout, Graphic Design, Typography, Photo editing, Research"
-                        }]} />
-
-                       
-                    </div>
-                     {/* Image grid container */}
-                     <div className={styles.imageGrid}>
-                            <div className={styles.imageItem}>
-                                <Image
-                                    src="/wallpap.png"
-                                    alt="Wallpaper"
-                                    width={500}
-                                    height={400}
-                                    className={styles.image1}
-                                />
-                            </div>
-                            <div className={styles.imageItem}>
-                                <Image
-                                    src="/bathroom.png"
-                                    alt="Bathroom design"
-                                    width={500}
-                                    height={400}
-                                    className={styles.image2}
-                                />
-                            </div>
+                    <div className={styles.content}>
+                        <h0 className={styles.title}>Eirene</h0>
+                        <h4 className={styles.subhead}>Designing and curating an Art Deco-inspired interior design magazine</h4>
+                        <p className={styles.emoticon}>(∩｀-´)⊃ ☆ﾟ.*･｡ﾟ</p>
+                        <div className={styles.textAndRoleContainer}>
+                            <p className={styles.body_text}>Eirene is an interior design magazine created to showcase the
+                                architectural and design style of Art Deco. Drawing from the monotony
+                                of boring modern interiors, this project
+                                aims to showcase a different architectural
+                                and design style while keeping readers engaged. The publication showcases the different textures,
+                                colours, and layouts of Art Deco. By bringing together all those design elements
+                                and combining them into one magazine, it keeps the reader interested and engaged while
+                                reading the publication.
+                            </p>
+                            <InfoBlock className={styles.role} items={[{
+                                title: "Year", bodyText: "September 2024 – October 2024"
+                            }, {
+                                title: "Role", bodyText: "Designer, Researcher"
+                            }, {
+                                title: "Skills", bodyText: "InDesign, Photoshop, Text layout, Graphic Design, Typography, Photo editing, Research"
+                            }]} />
                         </div>
+                    </div>
+                    {/* Image grid container */}
+                    <div className={styles.imageGrid}>
+                        <div className={styles.imageItem}>
+                            <Image
+                                src="/wallpap.png"
+                                alt="Wallpaper"
+                                width={500}
+                                height={400}
+                                className={styles.image1}
+                            />
+                        </div>
+                        <div className={styles.imageItem}>
+                            <Image
+                                src="/bathroom.png"
+                                alt="Bathroom design"
+                                width={700}
+                                height={600}
+                                className={styles.image2}
+                            />
+                        </div>
+                    </div>
                 </main>
                 <Footer />
             </div>
