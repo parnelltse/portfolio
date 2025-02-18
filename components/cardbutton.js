b0412f297962a6c591b3c6aaf42d8126c10140a0
parnelltse <parnell.tse@gmail.com>
2025-02-18 10:59:53 -0800

    fixing cursor

--- components/cardbutton.js
+++ components/cardbutton.js
@@ -2,14 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./cardbutton.module.css";
 
-export default function CardButton({ link, bgImage, frontImage, text, category }) {
+export default function CardButton({
+  link,
+  bgImage,
+  frontImage,
+  text,
+  category,
+}) {
   return (
     <div className={styles.container}>
       <Link href={link || "/default-page"} className={styles.linkWrapper}>
         <button className={styles.cardButton}>
-          {/* Image Container */}
           <div className={styles.imageContainer}>
-            {/* Background Image */}
             <Image
               src={bgImage || "/default-bg.png"}
               alt="Background image"
@@ -19,7 +23,6 @@ export default function CardButton({ link, bgImage, frontImage, text, category }
               style={{ position: "absolute" }}
             />
 
-            {/* Front Image */}
             <Image
               src={frontImage || "/default-front.png"}
               alt="Front image"
@@ -29,17 +32,14 @@ export default function CardButton({ link, bgImage, frontImage, text, category }
               style={{ position: "absolute" }}
             />
 
-            {/* Overlay & Shadow */}
             <div className={styles.overlay}></div>
             <div className={styles.shadow}></div>
           </div>
 
-          {/* Category Section */}
           <div className={styles.categoryContainer}>
             <p className={styles.category}>{category || "Default Category"}</p>
           </div>
 
-          {/* Text Container */}
           <div className={styles.textContainer}>
             <p className={styles.text}>{text || "Default Text"}</p>
           </div>
